eefba798b50684c0624c8dba677fa6c9d6cab233
Patrickx7 <p.jorgemoura@gmail.com>
2026-06-07 19:34:00 -0300

    Trabalhando com loops

--- ExemploCondicional.js
+++ ExemploCondicional.js
@@ -2,5 +2,7 @@ let saldo = 200.0;
 let valorSaque = 50.0;
 
 if (saldo >= valorSaque) {
-    console.log("Saque autorizado! Novo saldo: R$ " + (saldo - valorSaque))
+    console.log("Saque autorizado! Novo saldo: R$ " + (saldo - valorSaque));
+} else {
+    console.log("Saldo insuficiente. Saque negado.");
 }
