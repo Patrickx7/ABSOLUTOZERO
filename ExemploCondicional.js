a1f480207f1ca5dae5021a020875617bce0c6219
Patrickx7 <p.jorgemoura@gmail.com>
2026-06-07 19:20:06 -0300

    Entendendo a programar do ZERO

---
+++ ExemploCondicional.js
@@ -0,0 +1,6 @@
+let saldo = 200.0;
+let valorSaque = 50.0;
+
+if (saldo >= valorSaque) {
+    console.log("Saque autorizado! Novo saldo: R$ " + (saldo - valorSaque))
+}
---
+++ ExemploVariaveis.js
@@ -0,0 +1,12 @@
+let idade = 25;
+
+salario = 4500.50;
+
+nome = "Emerson";
+
+programador = true;
+
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Salário: " + salario);
+console.log("Programador: " + programador);
